855167de3f3b7b04a4239b9d3b86fc173fe264e4
leonemario320-cloud <leonemario320@gmail.com>
2026-03-25 14:29:00 +0100

    Satellite 3.0

--- app/components/OrbitalSatelliteMini.tsx
+++ app/components/OrbitalSatelliteMini.tsx
@@ -493,7 +493,7 @@ export default function OrbitalSatelliteMini({
                     position: absolute;
                     left: 50%;
                     top: 50%;
-                    transform: translate(-50%, 50px);
+                    transform: translate(-50%, 10px);
                     
                 }
                 
