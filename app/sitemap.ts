7bdd223b9e976248a2d0d1e63e037d27daa1fdda
leonemario320-cloud <leonemario320@gmail.com>
2026-03-28 03:34:21 +0100

    SiteMap

---
+++ app/sitemap.ts
@@ -0,0 +1,12 @@
+export default function sitemap() {
+    return [
+        {
+            url: "https://www.earthlivedata.com",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://www.earthlivedata.com/earth",
+            lastModified: new Date(),
+        },
+    ];
+}
